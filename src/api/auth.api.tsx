218cc66fef3aabbc8cbec0b9e662898a63034b89
nekaartajaya <nekaartajaya@gmail.com>
2023-02-02 13:02:29 +0800

    fix: axios token still stuck after logout

--- src/api/auth.api.tsx
+++ src/api/auth.api.tsx
@@ -15,7 +15,9 @@ import SsuAPI from './baseMusician';
 export const registerUser = async (
   registerProps: RegisterPropsType,
 ): Promise<RegisterResponseType> => {
-  const {data} = await SsuAPI().request<RegisterResponseType>({
+  const {data} = await SsuAPI({
+    cookie: registerProps.toString(),
+  }).request<RegisterResponseType>({
     url: '/register',
     method: 'POST',
     data: registerProps,
@@ -27,7 +29,9 @@ export const registerUser = async (
 export const loginUser = async (
   loginProps: LoginPropsType | LoginPhonePropsType,
 ): Promise<LoginResponseType> => {
-  const {data} = await SsuAPI().request<LoginResponseType>({
+  const {data} = await SsuAPI({
+    cookie: loginProps.toString(),
+  }).request<LoginResponseType>({
     url: '/login',
     method: 'POST',
     data: loginProps,
@@ -40,7 +44,9 @@ export const loginSso = async (
   user: string,
   registrationType: RegistrationType,
 ): Promise<LoginResponseType> => {
-  const {data} = await SsuAPI().request<LoginResponseType>({
+  const {data} = await SsuAPI({
+    cookie: user,
+  }).request<LoginResponseType>({
     url: '/login-sso',
     method: 'POST',
     data: {
@@ -54,7 +60,9 @@ export const loginSso = async (
 export const loginPhoneNumber = async (
   loginProps: LoginPropsType | LoginPhonePropsType,
 ): Promise<LoginResponseType> => {
-  const {data} = await SsuAPI().request<LoginResponseType>({
+  const {data} = await SsuAPI({
+    cookie: loginProps.toString(),
+  }).request<LoginResponseType>({
     url: '/login-phone-number',
     method: 'POST',
     data: loginProps,
@@ -81,7 +89,9 @@ export const confirmEmailOtpRegister = async (
   email: string,
   code: string,
 ): Promise<ConfirmEmailOTPRegisterResponseType> => {
-  const {data} = await SsuAPI().request<ConfirmEmailOTPRegisterResponseType>({
+  const {data} = await SsuAPI({
+    cookie: email,
+  }).request<ConfirmEmailOTPRegisterResponseType>({
     url: '/confirm-otp/email/register',
     method: 'POST',
     data: {
@@ -99,7 +109,9 @@ export const confirmSmsOtpLogin = async (
   code: string,
   context: string,
 ): Promise<ConfirmSmsOTPLoginResponseType> => {
-  const {data} = await SsuAPI().request<ConfirmSmsOTPLoginResponseType>({
+  const {data} = await SsuAPI({
+    cookie: phoneNumber,
+  }).request<ConfirmSmsOTPLoginResponseType>({
     url: '/confirm-otp/sms',
     method: 'POST',
     data: {
@@ -159,7 +171,9 @@ export const confirmEmailOtpForgotPassword = async (
   email: string,
   code: string,
 ): Promise<ConfirmEmailOTPRegisterResponseType> => {
-  const {data} = await SsuAPI().request<ConfirmEmailOTPRegisterResponseType>({
+  const {data} = await SsuAPI({
+    cookie: email,
+  }).request<ConfirmEmailOTPRegisterResponseType>({
     url: '/confirm-otp/email/register',
     method: 'POST',
     data: {
@@ -177,7 +191,9 @@ export const changePassword = async (
   code: string,
   password: string,
 ): Promise<LoginResponseType> => {
-  const {data} = await SsuAPI().request<LoginResponseType>({
+  const {data} = await SsuAPI({
+    cookie: email,
+  }).request<LoginResponseType>({
     url: '/confirm-change-password',
     method: 'POST',
     data: {
