e19d68cda4d4cc3fb67cc39629c7f0ba8f1665cb
ramadhanbambang <ramadhan.bambang@sunnysideup.io>
2023-02-08 17:13:20 +0700

    SM-00: add new icons

---
+++ src/assets/icon/Menu.icon.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import {View} from 'react-native';
+import Svg, {Path} from 'react-native-svg';
+import {widthPercentage} from '../../utils';
+import {SvgProps} from '../../interface/svg.interface';
+
+function MenuIcon({
+  width = widthPercentage(24),
+  height = widthPercentage(24),
+  fill = 'none',
+  style,
+}: SvgProps) {
+  return (
+    <View style={[{width, height}, style]}>
+      <Svg width={'100%'} height={'100%'} fill={fill} viewBox={'0 0 24 24'}>
+        <Path
+          d="M21 7.75H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75zM21 12.75H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75zM21 17.75H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75z"
+          fill="#8794AD"
+        />
+      </Svg>
+    </View>
+  );
+}
+
+export default MenuIcon;
