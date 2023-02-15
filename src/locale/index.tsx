41e36ca8f79ffe32de52de81589d5681deb55b76
kriptonhaz <kriptonhaz@gmail.com>
2023-02-16 01:40:25 +0700

    setup i18n

---
+++ src/locale/index.tsx
@@ -0,0 +1,26 @@
+import enMessages from './en.json';
+import zhMessages from './zh.json';
+
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+
+export const optionDetection = {
+  lookupLocalStorage: 'i18nextLng',
+};
+
+const resources = {
+  en: {translation: enMessages},
+  zh: {translation: zhMessages},
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  fallbackLng: 'zh',
+  detection: optionDetection,
+  compatibilityJSON: 'v3',
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
