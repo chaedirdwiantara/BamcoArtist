b84eec4a529fdbde8bcb241806c772d52e4ca61b
kriptonhaz <kriptonhaz@gmail.com>
2023-02-17 23:14:59 +0700

    change default language

--- src/locale/index.tsx
+++ src/locale/index.tsx
@@ -15,7 +15,7 @@ const resources = {
 
 i18n.use(initReactI18next).init({
   resources,
-  fallbackLng: 'zh',
+  fallbackLng: 'en',
   detection: optionDetection,
   compatibilityJSON: 'v3',
   interpolation: {
