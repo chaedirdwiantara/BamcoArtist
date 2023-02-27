ce2531a3770d424b2adfdbee54992c38923b7865
nekaartajaya_ssu <neka.artajaya@sunnysideup.io>
2023-02-27 15:51:11 +0800

    fix: search merch & event

--- src/screen/Search/ListResultEvent.tsx
+++ src/screen/Search/ListResultEvent.tsx
@@ -81,6 +81,7 @@ const styles = StyleSheet.create({
     marginTop: heightPercentage(8),
     width: '100%',
     height: '100%',
+    paddingBottom: heightPercentage(200),
   },
   ListContainer: {
     paddingVertical: heightPercentage(25),
