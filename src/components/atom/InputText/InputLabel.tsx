f9e82e878d412a082c07c7a712697888ef06eb3a
ramadhanbambang <ramadhan.bambang@sunnysideup.io>
2023-03-02 00:11:09 +0700

    SM-00: update font size

--- src/components/atom/InputText/InputLabel.tsx
+++ src/components/atom/InputText/InputLabel.tsx
@@ -199,6 +199,7 @@ const styles = StyleSheet.create({
     color: Color.Neutral[10],
     paddingVertical: heightPercentage(12),
     paddingRight: mvs(40),
+    fontSize: mvs(15),
     // lineHeight: Platform.OS === 'ios' ? mvs(20) : undefined,
   },
   containerErrorMsg: {
