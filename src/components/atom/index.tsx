357ac4a87e50081020bd4ab22db78daa28893967
Bambang Ramadhan <ramadhan.bambang@sunnysideup.io>
2023-03-20 15:51:48 +0700

    SM-00: Add square image with text

--- src/components/atom/index.tsx
+++ src/components/atom/index.tsx
@@ -11,6 +11,7 @@ import {SsuText} from './Text/SsuText';
 import {SsuInput} from './InputText/SsuInput';
 import {SelectBox} from './SelectBox/SelectBox';
 import SquareImage from './SquareImage/SquareImage';
+import SquareImageText from './SquareImage/SquareImageText';
 import SsuStatusBar from './StatusBar';
 import SsuToast from './Toast';
 import IconMore from './IconMore';
@@ -29,6 +30,7 @@ export {
   SsuText,
   SelectBox,
   SquareImage,
+  SquareImageText,
   SsuStatusBar,
   SsuToast,
   IconMore,
