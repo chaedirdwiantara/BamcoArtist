edb6c99836dfbcfca20c1068c4913c460f839dc1
Bambang Ramadhan <ramadhan.bambang@sunnysideup.io>
2023-03-20 20:50:12 +0700

    SM-00: Change background color

--- src/components/atom/ChipMoney/ChipMoney.tsx
+++ src/components/atom/ChipMoney/ChipMoney.tsx
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: Color.Pink.linear,
+    backgroundColor: Color.Success[400],
     borderRadius: 4,
     paddingHorizontal: ms(10),
     paddingVertical: mvs(5),
