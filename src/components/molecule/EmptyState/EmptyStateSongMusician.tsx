67f18adeaf0b5ca7d6ec4f93cff4d2e680e0fe3b
nekaartajaya_ssu <neka.artajaya@sunnysideup.io>
2023-04-04 22:29:42 +0800

    feat: create empty state for playlist home

--- src/components/molecule/EmptyState/EmptyStateSongMusician.tsx
+++ src/components/molecule/EmptyState/EmptyStateSongMusician.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import {View, Text, StyleSheet, ViewStyle, TextStyle} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import {mvs} from 'react-native-size-matters';
 import {heightResponsive, width} from '../../../utils';
 import Color from '../../../theme/Color';
 
 interface Props {
   text: string;
-  icon?: React.ReactNode;
-  hideIcon?: boolean;
-  containerStyle?: ViewStyle;
-  textStyle?: TextStyle;
+  height?: number;
 }
 
 export const EmptyStateSongMusician: React.FC<Props> = (props: Props) => {
-  const {text} = props;
+  const {text, height = 250} = props;
   return (
-    <View style={[styles.root]}>
+    <View style={[styles.root, {height: heightResponsive(height)}]}>
       <Text style={[styles.text]}>{text}</Text>
     </View>
   );
@@ -26,7 +23,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    height: heightResponsive(250),
   },
   text: {
     color: Color.Neutral[10],
