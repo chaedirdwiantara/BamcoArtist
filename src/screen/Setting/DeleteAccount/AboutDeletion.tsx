dd620c1df4ef11f0dccf96f2bcf3fc3271fb4be4
Bambang Ramadhan <ramadhan.bambang@sunnysideup.io>
2023-04-13 05:43:49 +0700

    SM-512: Add new screen => about deletion

---
+++ src/screen/Setting/DeleteAccount/AboutDeletion.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import {StyleSheet, View, Text, ScrollView} from 'react-native';
+import {useTranslation} from 'react-i18next';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+
+import {color, typography} from '../../../theme';
+import {TopNavigation} from '../../../components';
+import {RootStackParams} from '../../../navigations';
+import {listReason} from '../../../data/reasonsDeletion';
+import {ArrowLeftIcon, ArrowRightIcon} from '../../../assets/icon';
+import {MenuText} from '../../../components/atom/MenuText/MenuText';
+import {widthResponsive, heightPercentage, width} from '../../../utils';
+
+export const AboutDeletionScreen: React.FC = () => {
+  const {t} = useTranslation();
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParams>>();
+
+  const onPressGoBack = () => {
+    navigation.goBack();
+  };
+
+  const goToInputDeletion = (id: number, text: string) => {
+    navigation.navigate('InputDeletion', {id, text});
+  };
+
+  return (
+    <View style={styles.root}>
+      <TopNavigation.Type1
+        title={t('Setting.DeleteAccount.Title')}
+        leftIcon={<ArrowLeftIcon />}
+        itemStrokeColor={color.Neutral[10]}
+        leftIconAction={onPressGoBack}
+        containerStyles={{
+          marginBottom: heightPercentage(10),
+          paddingHorizontal: widthResponsive(15),
+        }}
+      />
+
+      <ScrollView
+        style={styles.containerContent}
+        showsHorizontalScrollIndicator={false}>
+        <Text style={[typography.Heading6, styles.title]}>
+          {t('Setting.DeleteAccount.About.TitleDeletion')}
+        </Text>
+        <Text style={[typography.Body2, styles.subtitle]}>
+          {t('Setting.DeleteAccount.About.FirstParagraph')}
+        </Text>
+        <Text style={[typography.Body2, styles.subtitle]}>
+          {t('Setting.DeleteAccount.About.SecondParagraph')}
+        </Text>
+        <Text style={[typography.Body2, styles.subtitle]}>
+          {t('Setting.DeleteAccount.About.ThirdParagraph')}
+        </Text>
+        <Text style={[typography.Heading6, styles.title]}>
+          {t('Setting.DeleteAccount.About.TitleReason')}
+        </Text>
+        <Text style={[typography.Body2, styles.subtitle]}>
+          {t('Setting.DeleteAccount.About.FourthParagraph')}
+        </Text>
+
+        {/* TODO: Need to be wired with API List Reasons */}
+        {listReason.map((val, i) => (
+          <MenuText.RightIcon
+            key={i}
+            text={t(val.text) || ''}
+            containerStyles={{marginTop: heightPercentage(15)}}
+            icon={<ArrowRightIcon stroke={color.Success[400]} />}
+            onPress={() => goToInputDeletion(val.id, val.text)}
+          />
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: color.Dark[800],
+  },
+  containerContent: {
+    width: width * 0.9,
+    alignSelf: 'center',
+  },
+  title: {
+    color: color.Neutral[10],
+    marginVertical: heightPercentage(13),
+  },
+  subtitle: {
+    color: color.Neutral[10],
+    marginBottom: heightPercentage(10),
+  },
+});
