db2416aa03a5f2925d4778d6e8cde75735747a4e
chaedir dwiantara <chaedirdwiantara@chaedirs-MacBook-Pro.local>
2023-07-17 15:22:44 +0700

    minor change

--- src/screen/Analytics/Album/AlbumFullScreen/Album.tsx
+++ src/screen/Analytics/Album/AlbumFullScreen/Album.tsx
@@ -1,4 +1,4 @@
-import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React, {useState} from 'react';
 import {MusicPink2Icon} from '../../../../assets/icon';
 import {DropDownFilter, Gap, ListCard} from '../../../../components';
