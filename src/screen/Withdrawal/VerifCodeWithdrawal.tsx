b8dbe696838c6da221e4fe2528e83658398a689b
Bambang Ramadhan <ramadhan.bambang@sunnysideup.io>
2023-09-27 10:22:29 +0700

    BEAM-924: Update wiring confirm otp bank & credit

--- src/screen/Withdrawal/VerifCodeWithdrawal.tsx
+++ src/screen/Withdrawal/VerifCodeWithdrawal.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {useTranslation} from 'react-i18next';
 import {mvs} from 'react-native-size-matters';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
+import {
+  Gap,
+  ModalSuccess,
+  SsuOTPInput,
+  SsuOTPTimer,
+  TopNavigation,
+} from '../../components';
 import {color, font} from '../../theme';
 import {ArrowLeftIcon} from '../../assets/icon';
 import {RootStackParams} from '../../navigations';
 import {width, widthPercentage} from '../../utils';
-import {profileStorage} from '../../hooks/use-storage.hook';
+import {profileStorage, storage} from '../../hooks/use-storage.hook';
 import {useWithdrawHook} from '../../hooks/use-withdraw.hook';
 import {KeyboardShift} from '../../components/molecule/KeyboardShift';
 import RenderMessage from '../../components/molecule/OtpInput/RenderMessage';
-import {Gap, SsuOTPInput, SsuOTPTimer, TopNavigation} from '../../components';
 
 type WithdrawalProps = NativeStackScreenProps<
   RootStackParams,
@@ -23,17 +29,56 @@ export const VerifCodeWithdrawalScreen: React.FC<WithdrawalProps> = ({
   route,
   navigation,
 }: WithdrawalProps) => {
-  const {type, data} = route.params;
-  const timer = 30;
+  const {type, data, idWithdraw} = route.params;
+
+  const timer = 59;
   const {t} = useTranslation();
   const email = profileStorage()?.email;
-  const {isError, isOtpValid, confirmOtp} = useWithdrawHook();
+  const {isError, isLoading, isOtpValid, confirmOtpBank, confirmOtpWithdraw} =
+    useWithdrawHook();
+  const [showModalSuccess, setShowModalSuccess] = useState<boolean>(false);
+  const [titleModal, setTitleModal] = useState<string>('');
+  const [subtitleModal, setSubtitleModal] = useState<string>('');
+  const [buttonText, setButtonText] = useState<string>('');
+
+  // to show modal success
+  useEffect(() => {
+    if (isOtpValid) {
+      setShowModalSuccess(true);
+      if (type === 'withdraw') {
+        // first time withdraw
+        if (data?.countWithdraw === 0) {
+          setTitleModal('Withdrawal.ModalSuccess.Title');
+          setSubtitleModal('Withdrawal.ModalSuccess.SubtitleFirstTime');
+          setButtonText('Withdrawal.ModalSuccess.ButtonText');
+        } else {
+          setTitleModal('Withdrawal.ModalSuccess.Title');
+          setSubtitleModal('Withdrawal.ModalSuccess.SubtitleNextWithdraw');
+          setButtonText('Withdrawal.ModalSuccess.ButtonText');
+        }
+      } else {
+        if (type === 'add') {
+          setTitleModal('Withdrawal.ModalSuccess.CreateWithdrawalAccount');
+          setSubtitleModal('');
+          setButtonText('General.Dismiss');
+        } else if (type === 'edit') {
+          setTitleModal('Withdrawal.ModalSuccess.EditWithdrawalAccount');
+          setSubtitleModal('');
+          setButtonText('General.Dismiss');
+        } else {
+          setTitleModal('Withdrawal.ModalSuccess.DeleteWithdrawalAccount');
+          setSubtitleModal('');
+          setButtonText('General.Dismiss');
+        }
+      }
+    }
+  }, [data, isLoading, isOtpValid, type]);
 
   const onPressGoBack = () => {
     navigation.goBack();
   };
 
-  const onCodeComplete = async (otp: string) => {
+  const verifBankAccount = async (otp: string) => {
     // type add & edit isActive = true, delete isActive = false
     // type edit must send previous bank id
     const payload = {
@@ -43,9 +88,31 @@ export const VerifCodeWithdrawalScreen: React.FC<WithdrawalProps> = ({
       previousBank: type === 'edit' ? data?.previousBankId || 0 : 0,
     };
     try {
-      await confirmOtp(payload);
-      navigation.navigate('Withdrawal');
+      await confirmOtpBank(payload);
     } catch (error) {}
+  };
+
+  const verifWithdrawReq = async (otp: string) => {
+    const payload = {
+      id: idWithdraw || 0,
+      code: otp,
+    };
+    try {
+      await confirmOtpWithdraw(payload);
+    } catch (error) {}
+  };
+
+  const onCodeComplete = async (otp: string) => {
+    type === 'withdraw' ? verifWithdrawReq(otp) : verifBankAccount(otp);
+  };
+
+  const onPressModalSuccess = () => {
+    if (type === 'withdraw') {
+      storage.set('withdrawIndex', 2);
+      navigation.navigate('TopUpCredit');
+    } else {
+      navigation.navigate('Withdrawal');
+    }
   };
 
   return (
@@ -85,12 +152,27 @@ export const VerifCodeWithdrawalScreen: React.FC<WithdrawalProps> = ({
             }}
           />
           {isError ? (
-            <RenderMessage otpSuccess={isOtpValid ? true : false} />
+            <RenderMessage
+              otpSuccess={isOtpValid ? true : false}
+              valMessage={t('Withdrawal.VerifCode.Rejected') || ''}
+            />
           ) : null}
 
-          <SsuOTPTimer action={() => {}} timer={timer} />
+          <SsuOTPTimer
+            action={() => {}}
+            timer={timer}
+            timerText={t('Withdrawal.VerifCode.ResendAfter') || ''}
+          />
           <Gap height={4} />
         </View>
+
+        <ModalSuccess
+          title={t(titleModal)}
+          subtitle={t(subtitleModal) || ''}
+          buttonText={t(buttonText)}
+          modalVisible={showModalSuccess}
+          onPress={onPressModalSuccess}
+        />
       </View>
     </KeyboardShift>
   );
