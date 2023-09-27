492f1ffa30dc05930270c0c7d15ab6563ebfbb1a
Bambang Ramadhan <ramadhan.bambang@sunnysideup.io>
2023-09-27 10:19:13 +0700

    BEAM-00: Add new utils to check if keyboard is visible

---
+++ src/utils/useKeyboard.tsx
@@ -0,0 +1,31 @@
+import {useEffect, useState} from 'react';
+import {Keyboard} from 'react-native';
+
+const useKeyboard = () => {
+  const [isKeyboardVisible, setKeyboardVisible] = useState(false);
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      () => {
+        setKeyboardVisible(true);
+      },
+    );
+
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => {
+        setKeyboardVisible(false);
+      },
+    );
+
+    return () => {
+      keyboardDidHideListener.remove();
+      keyboardDidShowListener.remove();
+    };
+  }, []);
+
+  return isKeyboardVisible;
+};
+
+export default useKeyboard;
