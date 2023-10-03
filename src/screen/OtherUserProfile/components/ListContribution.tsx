d8f040d0ddb0fa78c925877e28f4cae47899983b
Bambang Ramadhan <ramadhan.bambang@sunnysideup.io>
2023-10-03 10:43:42 +0700

    BEAM-1087: Fix handle Pagination List Contribution to Musician

--- src/screen/OtherUserProfile/components/ListContribution.tsx
+++ src/screen/OtherUserProfile/components/ListContribution.tsx
@@ -1,14 +1,15 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
-import {useQuery} from 'react-query';
 import {useTranslation} from 'react-i18next';
 import {FlashList} from '@shopify/flash-list';
 import {mvs} from 'react-native-size-matters';
 
-import {EmptyState} from '../../../components';
 import {heightPercentage} from '../../../utils';
+import {EmptyState} from '../../../components';
 import {useMusicianHook} from '../../../hooks/use-musician.hook';
 import MusicianSection from '../../../components/molecule/MusicianSection/MusicianSection';
+import {MusicianList} from '../../../interface/musician.interface';
+import LoadingSpinner from '../../../components/atom/Loading/LoadingSpinner';
 
 export interface ListContributonProps {
   uuid: string;
@@ -18,46 +19,86 @@ const ListContributionToMusician: FC<ListContributonProps> = (
   props: ListContributonProps,
 ) => {
   const {t} = useTranslation();
-  const {getListContribution} = useMusicianHook();
-  const {data: dataContribution, isLoading} = useQuery(
-    ['/list-contribution'],
-    () => getListContribution({fansUUID: props.uuid}),
-  );
+  const {useListContribution} = useMusicianHook();
+  const [dataContribution, setDataContribution] = useState<MusicianList[]>([]);
+  const [totalPage, setTotalPage] = useState<number>(1);
+  const {
+    data,
+    isLoading,
+    refetch,
+    isRefetching,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  } = useListContribution(totalPage, {fansUUID: props.uuid});
+
+  useEffect(() => {
+    if (data?.pages?.[0] !== undefined) {
+      setDataContribution(
+        data?.pages?.flatMap((page: any) => page?.data ?? []) ?? [],
+      );
+      const total = Math.ceil(data?.pages?.[0]?.meta.total ?? 1);
+      setTotalPage(total);
+    } else {
+      setDataContribution([]);
+      setTotalPage(1);
+    }
+  }, [data]);
+
+  const loadMore = () => {
+    if (hasNextPage) {
+      fetchNextPage();
+    }
+  };
+
+  useEffect(() => {
+    refetch();
+  }, []);
 
   return (
     <View style={styles.container}>
-      {isLoading ? null : dataContribution &&
-        dataContribution?.data.length > 0 ? (
-        <FlashList
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={styles.listContainer}
-          data={dataContribution.data}
-          renderItem={({item, index}) => (
-            <MusicianSection
-              musicianNum={(index + 1).toLocaleString('en-US', {
-                minimumIntegerDigits: 2,
-                useGrouping: false,
-              })}
-              musicianName={item?.fullname}
-              imgUri={
-                item.imageProfileUrls.length > 0
-                  ? item.imageProfileUrls[0].image
-                  : ''
-              }
-              containerStyles={styles.containerListContribution}
-              userId={item?.uuid}
-              followersCount={item?.followers}
-              activeMore={false}
-              point={item.credit}
+      <FlashList
+        data={dataContribution}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item: MusicianList) => item?.uuid?.toString()}
+        onTouchEnd={loadMore}
+        ListEmptyComponent={
+          !isLoading ? (
+            <EmptyState
+              text={t('Profile.Label.NoMusician') || ''}
+              containerStyle={{marginVertical: heightPercentage(30)}}
             />
-          )}
-          estimatedItemSize={150}
-        />
-      ) : (
-        <EmptyState
-          text={t('Profile.Label.NoMusicianOther') || ''}
-          containerStyle={{marginVertical: heightPercentage(30)}}
-        />
+          ) : (
+            <View style={styles.loadingContainer}>
+              <LoadingSpinner />
+            </View>
+          )
+        }
+        renderItem={({item, index}) => (
+          <MusicianSection
+            musicianNum={(index + 1).toLocaleString('en-US', {
+              minimumIntegerDigits: 2,
+              useGrouping: false,
+            })}
+            musicianName={item?.fullname}
+            imgUri={
+              item.imageProfileUrls.length > 0
+                ? item.imageProfileUrls[0].image
+                : ''
+            }
+            containerStyles={styles.containerListContribution}
+            userId={item?.uuid}
+            followersCount={item?.followers}
+            activeMore={false}
+            point={item.credit}
+          />
+        )}
+        estimatedItemSize={150}
+      />
+      {(isLoading || isRefetching || isFetchingNextPage) && (
+        <View style={styles.loadingContainer}>
+          <LoadingSpinner />
+        </View>
       )}
     </View>
   );
@@ -78,5 +119,9 @@ const styles = StyleSheet.create({
     marginTop: mvs(20),
     width: '100%',
     alignSelf: 'center',
+  },
+  loadingContainer: {
+    alignItems: 'center',
+    paddingTop: heightPercentage(20),
   },
 });
