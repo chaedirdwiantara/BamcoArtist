85a3aa2b5008d1c1ae5364b898784098876c8e32
Bambang Ramadhan <ramadhan.bambang@sunnysideup.io>
2023-10-20 17:37:08 +0700

    BEAM-1216: Add Adjustable Summary

--- src/data/home.tsx
+++ src/data/home.tsx
@@ -22,39 +22,39 @@ export interface OverviewCardType {
 export const listOverviewCard: OverviewCardType[] = [
   {
     id: 1,
+    amount: 1780,
+    title: 'Home.OverviewCard.SixthCard',
+    path: require('../assets/image/my_income.png'),
+  },
+  {
+    id: 2,
     amount: 35200,
     title: 'Home.OverviewCard.FirstCard',
     path: require('../assets/image/my_fans.png'),
   },
   {
-    id: 2,
+    id: 3,
     amount: 1200,
     title: 'Home.OverviewCard.SecondCard',
     path: require('../assets/image/my_public_post.png'),
   },
   {
-    id: 3,
+    id: 4,
     amount: 1500,
     title: 'Home.OverviewCard.ThirdCard',
     path: require('../assets/image/my_exclusive_post.png'),
   },
   {
-    id: 4,
+    id: 5,
     amount: 78,
     title: 'Home.OverviewCard.FourthCard',
     path: require('../assets/image/my_album.png'),
   },
   {
-    id: 5,
+    id: 6,
     amount: 12,
     title: 'Home.OverviewCard.FifthCard',
     path: require('../assets/image/my_songs.png'),
-  },
-  {
-    id: 6,
-    amount: 1780,
-    title: 'Home.OverviewCard.SixthCard',
-    path: require('../assets/image/my_income.png'),
   },
 ];
 
