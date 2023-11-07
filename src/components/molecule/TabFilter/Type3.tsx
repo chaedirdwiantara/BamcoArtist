7241f7561066d74f24025a878ffa3baf586502cd
Bambang Ramadhan <ramadhan.bambang@sunnysideup.io>
2023-11-07 13:20:56 +0700

    BEAM-1316: Add coachmark in tab home screen

--- src/components/molecule/TabFilter/Type3.tsx
+++ src/components/molecule/TabFilter/Type3.tsx
@@ -10,7 +10,16 @@ import {
 } from 'react-native';
 import {mvs} from 'react-native-size-matters';
 import {color, font} from '../../../theme';
+import {StepCopilot} from '../StepCopilot';
 import {heightResponsive, widthResponsive} from '../../../utils';
+import {
+  listNameTabAnalytic,
+  listNameTabSong,
+  listTextTabAnalytic,
+  listTextTabSong,
+  orderTabAnalytic,
+  orderTabSong,
+} from '../../../data/home';
 
 interface filterData {
   filterName: string;
@@ -18,16 +27,69 @@ interface filterData {
 
 interface TabFilterProps {
   filterData: Array<filterData>;
-  onPress: (params: string, index: number) => void;
+  onPress: (params: string, index: number, copilotName: string) => void;
   selectedIndex: number;
   containerStyle?: ViewStyle;
   flatlistContainerStyle?: ViewStyle;
   TouchableStyle?: ViewStyle;
   translation?: boolean;
+  showCopilot?: boolean;
+  tabType?: string;
+}
+
+interface ContentPropsType {
+  item: filterData;
+  index: number;
+  onPress: (params: string, index: number) => void;
+  selectedIndex: number;
+  TouchableStyle?: ViewStyle;
+  translation?: boolean;
+  showCopilot?: boolean;
 }
 
 const SelectedColor = color.Pink[100];
 const UnSelectedColor = color.Dark[50];
+
+const Content: React.FC<ContentPropsType> = ({
+  item,
+  index,
+  onPress,
+  selectedIndex,
+  TouchableStyle,
+  translation,
+}) => {
+  const {t} = useTranslation();
+  return (
+    <TouchableOpacity
+      style={[styles.tabStyle, TouchableStyle]}
+      onPress={() => onPress(item.filterName, index)}>
+      <Text
+        style={[
+          styles.TextStyle,
+          selectedIndex === index
+            ? {
+                fontFamily: font.InterBold,
+                fontWeight: '700',
+                color: SelectedColor,
+              }
+            : {
+                fontFamily: font.InterMedium,
+                fontWeight: '500',
+                color: UnSelectedColor,
+              },
+        ]}>
+        {translation ? t(item.filterName) : item.filterName}
+      </Text>
+      <View
+        style={{
+          borderBottomWidth: selectedIndex == index ? 2 : 0,
+          borderColor: SelectedColor,
+          marginTop: heightResponsive(10),
+          width: widthResponsive(36),
+        }}></View>
+    </TouchableOpacity>
+  );
+};
 
 const Type3: React.FC<TabFilterProps> = ({
   filterData,
@@ -37,8 +99,14 @@ const Type3: React.FC<TabFilterProps> = ({
   flatlistContainerStyle,
   TouchableStyle,
   translation,
+  showCopilot,
+  tabType,
 }) => {
   const {t} = useTranslation();
+  const newOnPress = (params: string, index: number, copilotName: string) => {
+    onPress(params, index, copilotName);
+  };
+
   return (
     <View style={[styles.tab, containerStyle]}>
       <FlatList
@@ -49,36 +117,54 @@ const Type3: React.FC<TabFilterProps> = ({
           styles.containerFlatlist,
           flatlistContainerStyle,
         ]}
-        renderItem={({item, index}) => (
-          <TouchableOpacity
-            style={[styles.tabStyle, TouchableStyle]}
-            onPress={() => onPress(item.filterName, index)}>
-            <Text
-              style={[
-                styles.TextStyle,
-                selectedIndex === index
-                  ? {
-                      fontFamily: font.InterBold,
-                      fontWeight: '700',
-                      color: SelectedColor,
+        renderItem={({item, index}) => {
+          if (showCopilot) {
+            // title of coachmark
+            const listNameTab =
+              tabType === 'song' ? listNameTabSong : listNameTabAnalytic;
+            // subtitle of coachmark
+            const listTextTab =
+              tabType === 'song' ? listTextTabSong : listTextTabAnalytic;
+            // order/step of coachmark
+            const orderTabCoachmark =
+              tabType === 'song' ? orderTabSong : orderTabAnalytic;
+
+            return (
+              <StepCopilot
+                children={
+                  <Content
+                    item={item}
+                    index={index}
+                    onPress={(item, index) =>
+                      newOnPress(item, index, listNameTab[index])
                     }
-                  : {
-                      fontFamily: font.InterMedium,
-                      fontWeight: '500',
-                      color: UnSelectedColor,
-                    },
-              ]}>
-              {translation ? t(item.filterName) : item.filterName}
-            </Text>
-            <View
-              style={{
-                borderBottomWidth: selectedIndex == index ? 2 : 0,
-                borderColor: SelectedColor,
-                marginTop: heightResponsive(10),
-                width: widthResponsive(36),
-              }}></View>
-          </TouchableOpacity>
-        )}
+                    selectedIndex={selectedIndex}
+                    TouchableStyle={TouchableStyle}
+                    translation={translation}
+                    showCopilot={showCopilot}
+                  />
+                }
+                // have to calculate when it appears based on figma
+                // ex: tab fans appear in step 6, so the order is 6
+                order={orderTabCoachmark[index]}
+                name={t(listNameTab[index])}
+                text={t(listTextTab[index])}
+              />
+            );
+          } else {
+            return (
+              <Content
+                item={item}
+                index={index}
+                onPress={newOnPress}
+                selectedIndex={selectedIndex}
+                TouchableStyle={TouchableStyle}
+                translation={translation}
+                showCopilot={showCopilot}
+              />
+            );
+          }
+        }}
       />
     </View>
   );
