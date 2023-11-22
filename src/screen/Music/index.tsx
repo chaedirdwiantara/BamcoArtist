6608a76fa8b9a15b53e35bddb1c9f132008a9bb5
Bambang Ramadhan <ramadhan.bambang@sunnysideup.io>
2023-11-22 10:53:33 +0700

    BEAM-1441: Create music component to share to socmed

--- src/screen/Music/index.tsx
+++ src/screen/Music/index.tsx
@@ -1,39 +1,57 @@
+import React, {FC, useCallback, useEffect, useRef, useState} from 'react';
 import {
-  Image,
-  StyleSheet,
   View,
-  Dimensions,
   Text,
+  Image,
   Animated,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
 } from 'react-native';
-import React, {FC, useEffect, useRef, useState} from 'react';
-import {color, font} from '../../theme';
-import {ModalDonate, ModalSuccessDonate, SsuStatusBar} from '../../components';
-import {heightResponsive, widthResponsive} from '../../utils';
-import MusicControl from './MusicControl';
-import TopNav from './TopNav';
-import Footer from './Footer';
-import TitleAndDonate from './TitleAndDonate';
-import {Slider} from '@miblanchard/react-native-slider';
+import Share from 'react-native-share';
+import {useTranslation} from 'react-i18next';
+import ViewShot from 'react-native-view-shot';
 import {mvs} from 'react-native-size-matters';
-import {songs, SongsProps} from '../../data/music';
-import {usePlayerHook} from '../../hooks/use-player.hook';
-import {RootStackParams} from '../../navigations';
+import ImageColors from 'react-native-image-colors';
+import {Slider} from '@miblanchard/react-native-slider';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {useCreditHook} from '../../hooks/use-credit.hook';
+
+import {
+  Gap,
+  ModalDonate,
+  SsuStatusBar,
+  ModalSuccessDonate,
+} from '../../components';
+import TopNav from './TopNav';
+import {color, font} from '../../theme';
+import MusicControl from './MusicControl';
+import {SongsProps} from '../../data/music';
+import {Beamco2Logo} from '../../assets/logo';
+import TitleAndDonate from './TitleAndDonate';
+import {RootStackParams} from '../../navigations';
+import {useSongHook} from '../../hooks/use-song.hook';
+import {usePlayerHook} from '../../hooks/use-player.hook';
 import {profileStorage} from '../../hooks/use-storage.hook';
+import {heightResponsive, widthResponsive} from '../../utils';
+import {CoinCIcon, LoveIcon, ShareIcon} from '../../assets/icon';
 
 export const {width} = Dimensions.get('screen');
 
 type MusicProps = NativeStackScreenProps<RootStackParams, 'MusicPlayer'>;
 
 export const MusicPlayer: FC<MusicProps> = ({navigation}: MusicProps) => {
+  const {t} = useTranslation();
   const scrollX = useRef(new Animated.Value(0)).current;
   const {playerProgress, currentTrack, seekPlayer} = usePlayerHook();
-  const {creditCount, getCreditCount} = useCreditHook();
+  const {setLikeSong, setUnlikeSong} = useSongHook();
   const [modalDonate, setModalDonate] = useState<boolean>(false);
   const [modalSuccessDonate, setModalSuccessDonate] = useState<boolean>(false);
   const [trigger2ndModal, setTrigger2ndModal] = useState<boolean>(false);
+  const [colors, setColors] = useState();
+  const [screenshot, setScreenshot] = useState<string>('');
+  const [showShareImage, setShowShareImage] = useState<boolean>(true);
+  const [isLiked, setIsLiked] = useState<boolean>(currentTrack?.isLiked);
+
   // const [songIndex, setSongIndex] = useState<number>(0);
 
   // TODO: will gonna use it when swipe on the song is applied
@@ -47,8 +65,19 @@ export const MusicPlayer: FC<MusicProps> = ({navigation}: MusicProps) => {
   const {showMiniPlayerOnly} = usePlayerHook();
 
   useEffect(() => {
-    if (modalDonate) getCreditCount();
-  }, [modalDonate]);
+    const url = currentTrack?.artwork;
+    async function fetchMyAPI() {
+      const result = await ImageColors.getColors(url, {
+        fallback: '#228B22',
+        cache: true,
+        key: url,
+      });
+
+      setColors(result);
+    }
+
+    fetchMyAPI();
+  }, []);
 
   const RenderSongs = (item: SongsProps, index: number) => {
     return (
@@ -88,6 +117,28 @@ export const MusicPlayer: FC<MusicProps> = ({navigation}: MusicProps) => {
     navigation.goBack();
   };
 
+  const onCapture = useCallback(uri => {
+    setScreenshot(uri);
+    setShowShareImage(false);
+  }, []);
+
+  const shareMusic = () => {
+    const shareOptions = {
+      url: screenshot,
+      social: Share.Social.INSTAGRAM,
+    };
+
+    Share.open(shareOptions);
+  };
+
+  const likeOnPress = async (isLikedSong: boolean) => {
+    isLikedSong
+      ? await setUnlikeSong({id: currentTrack?.id})
+      : await setLikeSong({id: currentTrack?.id});
+
+    setIsLiked(!isLikedSong);
+  };
+
   return (
     <View style={styles.container}>
       <SsuStatusBar type="black" />
@@ -119,9 +170,7 @@ export const MusicPlayer: FC<MusicProps> = ({navigation}: MusicProps) => {
         <TitleAndDonate
           title={currentTrack?.title ?? ''}
           artist={currentTrack?.artist ?? ''}
-          coinOnPress={coinOnPress}
           artistOnPress={artistOnPress}
-          showDonate={currentTrack?.musicianId !== profileStorage()?.uuid}
         />
       </View>
       {/* slider */}
@@ -160,8 +209,61 @@ export const MusicPlayer: FC<MusicProps> = ({navigation}: MusicProps) => {
         <MusicControl />
       </View>
       <View style={styles.footerStyle}>
-        <Footer />
+        <TouchableOpacity style={styles.containerShare} onPress={shareMusic}>
+          <ShareIcon width={mvs(19)} height={mvs(19)} />
+          <Text style={styles.textShare}>{t('General.Share.Share')}</Text>
+        </TouchableOpacity>
+        <View style={styles.containerShare}>
+          <TouchableOpacity onPress={() => likeOnPress(isLiked)}>
+            <LoveIcon
+              fill={isLiked ? color.Pink[100] : 'none'}
+              stroke={isLiked ? 'none' : color.Neutral[10]}
+              width={mvs(24)}
+              height={mvs(24)}
+            />
+          </TouchableOpacity>
+          {currentTrack?.musicianId !== profileStorage()?.uuid && (
+            <>
+              <Gap width={widthResponsive(15)} />
+              <TouchableOpacity onPress={coinOnPress}>
+                <CoinCIcon
+                  width={mvs(22)}
+                  height={mvs(22)}
+                  fill={color.Neutral[10]}
+                />
+              </TouchableOpacity>
+            </>
+          )}
+        </View>
       </View>
+
+      <Gap height={mvs(20)} />
+      {showShareImage && (
+        <ViewShot onCapture={onCapture} captureMode="mount">
+          <View
+            style={[
+              styles.containerMusic,
+              {
+                backgroundColor:
+                  colors?.darkVibrant === '#000000'
+                    ? colors?.darkMuted
+                    : colors?.darkVibrant,
+              },
+            ]}>
+            <Beamco2Logo />
+            <View style={styles.containerImage}>
+              <Image
+                source={{
+                  uri: currentTrack?.artwork,
+                }}
+                style={styles.imageStyle}
+              />
+            </View>
+            <Text style={styles.songTitle}>{currentTrack?.title}</Text>
+            <Text style={styles.artistName}>{currentTrack?.artist}</Text>
+          </View>
+        </ViewShot>
+      )}
 
       {/* modal */}
       <ModalDonate
@@ -222,6 +324,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: widthResponsive(24),
   },
   footerStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     marginTop: heightResponsive(39),
     paddingHorizontal: widthResponsive(24),
     marginBottom: heightResponsive(24),
@@ -236,5 +340,49 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     fontSize: mvs(10),
     color: color.Neutral[10],
+  },
+  containerShare: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textShare: {
+    fontFamily: font.InterMedium,
+    fontWeight: '500',
+    fontSize: mvs(12),
+    color: color.Neutral[10],
+    marginLeft: widthResponsive(8),
+  },
+  containerMusic: {
+    alignSelf: 'center',
+    paddingVertical: mvs(20),
+    paddingHorizontal: mvs(17),
+    backgroundColor: '#000',
+    borderRadius: mvs(8),
+  },
+  containerImage: {
+    width: width * 0.65,
+    height: width * 0.65,
+  },
+  imageStyle: {
+    width: '100%',
+    height: '100%',
+    borderRadius: mvs(8),
+  },
+  songTitle: {
+    fontFamily: font.InterRegular,
+    fontWeight: '700',
+    color: color.Neutral[10],
+    fontSize: mvs(20),
+    maxWidth: width * 0.6,
+    marginTop: mvs(10),
+  },
+  artistName: {
+    fontFamily: font.InterRegular,
+    fontWeight: '600',
+    color: color.Neutral[10],
+    fontSize: mvs(16),
+    maxWidth: width * 0.6,
+    marginTop: mvs(10),
   },
 });
