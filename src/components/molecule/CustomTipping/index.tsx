ee8552f5fcacb8fae96ff3ca2d20e234ab8a9efb
Bambang Ramadhan <ramadhan.bambang@sunnysideup.io>
2023-11-24 09:09:54 +0700

    BEAM-1485: Update component & add new translates

--- src/components/molecule/CustomTipping/index.tsx
+++ src/components/molecule/CustomTipping/index.tsx
@@ -8,39 +8,53 @@ import {
   TouchableOpacity,
   ImageSourcePropType,
 } from 'react-native';
-import {color, font} from '../../../theme';
+import {useTranslation} from 'react-i18next';
 import {ms, mvs} from 'react-native-size-matters';
+import {color, font} from '../../../theme';
 
 interface ChipCreditProps {
   source: ImageSourcePropType;
   credit: number;
   onPress: (credit: number) => void;
+  selectedChip: number;
   chipStyles?: ViewStyle;
 }
 
 interface CustomTippingProps {
+  selectedChip: number;
   onPress: (credit: number) => void;
   chipStyles?: ViewStyle;
   containerStyles?: ViewStyle;
 }
 
 const ChipCredit: React.FC<ChipCreditProps> = ({
+  selectedChip,
   source,
   credit,
   onPress,
   chipStyles,
 }) => {
+  const {t} = useTranslation();
+  const isActive = selectedChip === credit;
+  const isActiveStyle = {
+    backgroundColor: isActive ? 'rgba(196, 163, 54, 0.4)' : 'transparent',
+    borderColor: isActive ? '#C4A436' : '#333B60',
+  };
+
   return (
     <TouchableOpacity
-      style={[styles.containerItem, chipStyles]}
+      style={[styles.containerItem, isActiveStyle, chipStyles]}
       onPress={() => onPress(credit)}>
       <Image source={source} />
-      <Text style={styles.credit}>{credit + ' Credit'}</Text>
+      <Text style={styles.credit}>
+        {isActive ? t('LiveTipping.NowActive') : t('LiveTipping.Credit')}
+      </Text>
     </TouchableOpacity>
   );
 };
 
 export const CustomTipping: React.FC<CustomTippingProps> = ({
+  selectedChip,
   onPress,
   chipStyles,
   containerStyles,
@@ -59,6 +73,7 @@ export const CustomTipping: React.FC<CustomTippingProps> = ({
           credit={val}
           source={chipSrc[index]}
           chipStyles={chipStyles}
+          selectedChip={selectedChip}
           onPress={onPress}
         />
       ))}
@@ -68,18 +83,22 @@ export const CustomTipping: React.FC<CustomTippingProps> = ({
 
 const styles = StyleSheet.create({
   root: {
-    flexDirection: 'row',
     alignSelf: 'center',
   },
   containerItem: {
     justifyContent: 'center',
     alignItems: 'center',
     marginHorizontal: ms(10),
+    borderWidth: mvs(1),
+    borderRadius: mvs(6),
+    paddingVertical: mvs(12),
+    paddingHorizontal: ms(5),
+    marginBottom: mvs(10),
   },
   credit: {
     fontFamily: font.InterMedium,
     color: color.Neutral[10],
-    fontSize: mvs(10),
-    marginTop: mvs(10),
+    fontSize: mvs(7),
+    marginTop: mvs(5),
   },
 });
